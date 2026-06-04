f749a072ba2c91ac8b019635d0fbf3074c0aa7ea
tylermachado <3586466+tylermachado@users.noreply.github.com>
2026-06-04 09:06:09 -0400

    footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,9 @@
-<footer class="p-4 mt-8 text-center text-sm text-gray-900 bg-gray-300">
-    <p>Brought to you by <a href="//tylermachado.com" class="underline">Tyler Machado</a>, who thinks you, Boston soccer fan, should join the <a href="//bostonisa.com" class="underline">Boston ISA</a>.</p>
+<footer class="p-4 mt-8 text-left text-sm text-gray-900 bg-gray-300">
+    <p>Brought to you by Tyler Machado, who is:    </p>
+        <ol class="list-decimal list-inside ml-4">
+            <li>not affiliated with FIFA, the Boston Host Committee, or any watch party organizers.</li>
+            <li><a href="//tylermachado.com" class="underline">available for design, development and adjacent work.</a></li>
+            <li>thinks you, Boston soccer fan, should join the <a href="//bostonisa.com" class="underline">Boston ISA</a>.</li>
+        </ol>
+
 </footer>
